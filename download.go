cfc1b5aaebbe81dffa94fbd87cb9b76561a0263e
Christoph Armster <christoph1703@gmail.com>
2022-03-06 14:14:02 +0100

    remove existing tar file before downloading

--- download.go
+++ download.go
@@ -14,15 +14,20 @@ func downloadTAR(version string) (string, error) {
 	url := fmt.Sprintf(downloadURL, version, runtime.GOOS, runtime.GOARCH)
 	path := fmt.Sprintf(downloadFile, version, runtime.GOOS, runtime.GOARCH)
 
+	err := os.RemoveAll(path)
+	if err != nil {
+		return "", err
+	}
+
 	f, err := os.Create(path)
 	if err != nil {
-		return path, err
+		return "", err
 	}
 	defer f.Close()
 
 	res, err := http.Get(url)
 	if err != nil {
-		return path, err
+		return "", err
 	}
 	defer res.Body.Close()
 
@@ -32,5 +37,9 @@ func downloadTAR(version string) (string, error) {
 	)
 
 	_, err = io.Copy(io.MultiWriter(f, bar), res.Body)
-	return path, err
+	if err != nil {
+		return "", err
+	}
+
+	return path, nil
 }
